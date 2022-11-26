c9bfb477186b86d038b95d279b8e536e814b3ecf
seungho <seungho1216@gmail.com>
2022-11-27 00:14:36 +0900

    excel list extracting

--- src/main/java/com/example/exceldownload/Product.java
+++ src/main/java/com/example/exceldownload/Product.java
@@ -2,6 +2,7 @@ package com.example.exceldownload;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
   @Id
@@ -21,4 +23,15 @@ public class Product {
   private String description;
   private Integer price;
   private LocalDateTime expireDate;
+
+  public static Product create(String name,
+                               String description,
+                               Integer price){
+    Product product = new Product();
+    product.setName(name);
+    product.setDescription(description);
+    product.setPrice(price);
+    product.setExpireDate(LocalDateTime.now().plusMonths(1L));
+    return product;
+  }
 }
--- src/main/java/com/example/exceldownload/ProductService.java
+++ src/main/java/com/example/exceldownload/ProductService.java
@@ -1,11 +1,13 @@
 package com.example.exceldownload;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -15,14 +17,68 @@ public class ProductService {
   private final ProductMapper productMapper;
 
   public Map<String, Object> getExcelList() {
-    // 엑셀에 저장할 데이터
-    List<ProductDTO> excelDataList =
-      productMapper.toDTO(productRepository.findAll(Sort.by(Sort.Direction.DESC, "id")));
+    int page = 0;
+    int size = 10000;
+    Page<ProductDTO> productDTOS = getProductList(PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "id")));
+
     // 엑셀에 저장할 row 수
-    int dataSize = excelDataList.size();
-    // 엑셀 헤더 키 설정
-    List<String> headerKeys = List.of("id", "name", "description", "price", "expireDate");
+    int rowSize = productDTOS.getContent().size();
+    int totalPages = productDTOS.getTotalPages();
+    /*
+     // 엑셀 헤더 키 설정
+      필드값을 자바 메소드를 활용해서 뽑는 방법은 아래를 참조
+      https://roytuts.com/handling-large-data-writing-to-excel-using-sxssf-apache-poi/
+     */
+    List<String> headerKeys = List.of("id", "name", "description", "price", "expireDate"); // 이 자체가 헤더 키이자 헤더이다
+    List<Map<String, Object>> headerKeysMap = new ArrayList<>();
 
+    for (int i = 0; i < totalPages; i++) {
+      Page<ProductDTO> pagedExcelData = getProductList(PageRequest.of(i, size, Sort.by(Sort.Direction.DESC, "id")));
 
+      // 헤더 키에 1:1 매핑, 만개의 리스트 == 만개의 로우
+      for (ProductDTO excelData : pagedExcelData) {
+        Map<String, Object> tempMap = new HashMap<>();
+        tempMap.put("id", excelData.getId());
+        tempMap.put("name", excelData.getName());
+        tempMap.put("description", excelData.getDescription());
+        tempMap.put("price", excelData.getPrice());
+        tempMap.put("expireDate", excelData.getExpireDate());
+
+        headerKeysMap.add(tempMap);
+      }
+    }
+
+    // 헤더당 셀 너비 설정
+    List<String> widths = Arrays.asList("10", "20", "30", "15", "20");
+
+    // 파일명 설정
+    String fileName = "EXAMPLE_EXCEL";
+
+    Map<String, Object> excelMap = new HashMap<>();
+    excelMap.put("headersKeys", headerKeys); // 헤더 정보
+    excelMap.put("widths", widths); // 칼럼 너비
+    excelMap.put("headerKeysMap", headerKeysMap); // 헤더에 따른 정보 매핑(리스트 사이즈만큼 로우 나옴)
+    excelMap.put("fileName", fileName);
+    return excelMap;
+  }
+
+  public Page<ProductDTO> getProductList(Pageable pageable) {
+    // 엑셀에 저장할 데이터
+    return productRepository.findAll(pageable).map(productMapper::toDTO);
+  }
+
+  public ProductDTO createProduct(ProductDTO productDTO) {
+    return Optional.of(Product.create(
+        productDTO.getName(),
+        productDTO.getDescription(),
+        productDTO.getPrice()))
+      .map(productRepository::save)
+      .map(productMapper::toDTO)
+      .get();
   }
 }
+/*
+ * 흐름정리
+ * 1. 페이징 된 데이터 넘기기(예를 들어 만개 씩 페이징/ 총 데이터는 3만5천)
+ * 2. 파라미터로 시작값 넘기기(예를 들어 0, 10000, 20000, 30000 % max row -> 처음인지 아닌지 분간 및 rowNo 설정 영향)
+ * 3. 만약 파라미터로 시작값이 아닌 페이지 번호가 들어오면 x 갯수를 해주면 시작값일 것이다. */
